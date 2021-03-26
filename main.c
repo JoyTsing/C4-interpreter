aa4582053dbba7b320a3c8fbf78cb4976f4fa3b0
joyTsing <792566033@qq.com>
2021-03-26 11:22:05 +0800

    function declartion

--- main.c
+++ main.c
@@ -318,6 +318,140 @@ void match(int tk){
     }
 }
 
+/*
+ |    ....       | high address
+ +---------------+
+ | arg: param_a  |    new_bp + 3
+ +---------------+
+ | arg: param_b  |    new_bp + 2
+ +---------------+
+ |return address |    new_bp + 1
+ +---------------+
+ | old BP        | <- new BP
+ +---------------+
+ | local_1       |    new_bp - 1
+ +---------------+
+ | local_2       |    new_bp - 2
+ +---------------+
+ |    ....       |  low address
+ */
+int index_of_bp;//new_bp
+
+void function_parameter(){
+    int type;
+    int params=0;
+    while(token!=')'){
+        type=INT;
+        if(token==Int){
+            match(Int);
+        }else if(token=Char){
+            type=CHAR;
+            match(Char);
+        }
+
+        while(token==Mul){
+            match(Mul);
+            type=type+PTR;
+        }
+        if(token!=Id){
+            printf("%d: bad parameter declartion\n",line);
+            exit(-1);
+        }
+        if(current_id[Class]==Loc){
+            printf("%d: duplicate paramter declartion\n",line);
+            exit(-1);
+        }
+
+        match(Id);
+
+        //
+        current_id[BClass]=current_id[Class];current_id[Class]=Loc;
+        current_id[BType]=current_id[Type];current_id[Type]=type;
+        current_id[BType]=current_id[Value];current_id[Value]=params++;
+
+        if(token==','){
+            match(',');
+        }
+    }
+    index_of_bp=params+1;
+}
+
+void function_body(){
+    //type func_name(....){....}
+    //                 -->|    |<--
+    //
+
+    int pos_local;
+    int type;
+    pos_local=index_of_bp;
+
+    while(token==Int||token==Char){
+        basetype=(token==Int)?INT:CHAR;
+        match(token);
+
+        while(token!=';'){
+            type=basetype;
+            while(token==Mul){
+                match(Mul);
+                type=type+PTR;
+            }
+
+            if(token!=Id){
+                printf("%d: bad local declartion\n",line);
+                exit(-1);
+            }
+
+            if(current_id[Class]==Loc){
+                printf("%d: duplicate local declartion\n",line);
+                exit(-1);
+            }
+
+            match(Id);
+
+            current_id[BClass]=current_id[Class];current_id[Class]=Loc;
+            current_id[BType]=current_id[Type];current_id[Type]=type;
+            current_id[BType]=current_id[Value];current_id[Value]=++pos_local;
+
+            if(token==','){
+                match(',');
+            }
+        }
+        match(';');
+
+    }
+
+    //生成汇编代码
+    *++text=ENT;
+    *++text=pos_local-index_of_bp;
+
+    while(token!='}'){
+        statement();
+    }
+    *++text=LEV;
+
+}
+
+void function_declartion(){
+    //type func_name (....){....}
+    //              |  this
+    match('(');
+    function_parameter();
+    match(')');
+    match('{');
+    function_body();
+
+    //将符号表中的信息恢复成全局的信息
+    current_id=symbols;
+    while(current_id[Token]){
+        if(current_id[Class]==Loc){
+            current_id[Class]=current_id[BClass];
+            current_id[Type]=current_id[BType];
+            current_id[Value]=current_id[BValue];
+        }
+        current_id=current_id+IdSize;
+    }
+}
+
 void enum_declartion(){
     //enum [id] {a=1,b=3};
     int i;
